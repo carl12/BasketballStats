7a8b3027921324a460be3dd9cdb0dabc46b48d4d
carl12 <carl17r@gmail.com>
2019-05-06 20:22:27 -0700

    On hover show stat

--- client/src/components/AbsoluteChart.jsx
+++ client/src/components/AbsoluteChart.jsx
@@ -12,31 +12,72 @@ const Player = styled.div`
   background-color:${props => props.color};
   width:${props => props.width};
   height:${props => props.height}
+  transition: all 0.2s ease-in-out;
   &:hover {
-    background-color:blue;
+    box-shadow: 0 5px 15px rgba(145, 92, 182, .4);
   }
   border: 1px solid black;
 `;
 
-const ChartContainer = ({ width, height, data }) => {
-  let total = data.reduce((sum, player) => player.amount + sum, 0);
-  console.log(width, height);
+class PlayerEntry extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hovered: false };
+    this.onHover = this.onHover.bind(this);
+    this.offHover = this.offHover.bind(this);
+  }
+
+  onHover() {
+    this.setState({ hovered: true });
+  }
+  
+  offHover() {
+    this.setState({ hovered: false });
+  }
+
+  render() {
+    const {
+      overallWidth,
+      overallHeight,
+      total,
+      value,
+      name
+    } = this.props;
+    const { hovered } = this.state;
+    return (
+      <Player
+        width={`${overallWidth  }px`}
+        height={`${overallHeight * value / total - 2}px`}
+        onMouseEnter={this.onHover}
+        onMouseOut={this.offHover}
+        onBlur={this.offHover}
+      >
+        {name}
+        {hovered && `: ${value}`}
+      </Player>
+    );
+  }
+}
+
+const ChartContainer = ({ width, height, data, teamName, statName }) => {
+  const total = data.reduce((sum, player) => player.amount + sum, 0);
   return (
     <div>
-      <asdf> helloo</asdf>"
-      <Chart width={`${width}px`} height={`${height}px`} >
-        {data.map(player => makeChart(width, height, total, player.amount, player.name))}  
+      {`${teamName}: ${total.toFixed(2)} ${statName}`}
+      <Chart width={`${width}px`} height={`${height}px`}>
+        {data.map(player => (
+          <PlayerEntry
+            overallWidth={width}
+            overallHeight={height}
+            total={total}
+            value={player.amount}
+            name={player.name}
+          />
+        ))}
       </Chart>
     </div>
-)};
+  )
+};
 
-function makeChart(overallWidth, overallHeight, total, value, name) {
-  console.log('amount', value);
-  return <Player 
-          width={`${overallWidth * 0.9}px`} 
-          height={`${overallHeight * value/total - 2}px`}
-        >
-          {name}
-        </Player>
-}
+
 export default ChartContainer;
