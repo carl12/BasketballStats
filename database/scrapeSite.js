a171efa48d7895fe6931a63fc867b9575fd6f97e
carl12 <carl17r@gmail.com>
2019-06-11 20:00:00 -0700

    Add puppeteer to save pages; Adjust scrape based on those pages

--- database/scrapeSite.js
+++ database/scrapeSite.js
@@ -1,13 +1,20 @@
 const puppeteer = require('puppeteer');
 const fs = require('fs');
+const path = require('path');
+const htmlPath = i => path.join(__dirname, 'dataPages', `page${i}.html`);
+const pngPath = i => path.join(__dirname, 'dataPages', `img${i}.png`);
 
-(async () => {
+module.exports = (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   page.setViewport({ width: 1920, height: 1080 });
   await page.goto('https://stats.nba.com/leaders/?Season=2018-19&SeasonType=Regular Season&PerMode=Totals');
-  await page.screenshot({ path: 'example.png' });
-  const content = await page.content();
-  fs.writeFile('test.html', content);
-  await browser.close();
+  for (let i = 1; i <= 11; i += 1) {
+    await page.screenshot({ path: pngPath(i) });
+    const content = await page.content();
+    fs.writeFile(htmlPath(i), content, () => {});
+    await page.click('.stats-table-pagination__next');
+  }
+  console.log('puppeteer done');
+  return browser.close();
 })();
--- database/scrapper.js
+++ database/scrapper.js
@@ -2,41 +2,57 @@ const fs = require('fs');
 const path = require('path')
 const cheerio = require('cheerio');
 const { promisify } = require('util');
+const promise = require('./scrapeSite');
+promise.then(scrapeHtmlAndWrite);
 
 const read = promisify(fs.readFile);
 const write = promisify(fs.writeFile);
 const append = promisify(fs.appendFile);
 
 const files = [];
-for (let i = 1; i <= 11; i += 1) {
-  files.push(`nba${i}.html`);
-}
 
-
-let a = read(path.join(__dirname, 'dataPages', `nba1.html`))
-  .then(cleanInput)
-  .then(arr => writeToCSV(arr, false))
-  .then(() => console.log('done'));
-
-for (let i = 1; i < files.length; i++) {
-  a.then(() => read(path.join(__dirname, 'dataPages', files[i])))
+function scrapeHtmlAndWrite() {
+  console.log('beginning scrape')
+  let a = read(path.join(__dirname, 'dataPages', `page1.html`))
     .then(cleanInput)
-    .then(arr => writeToCSV(arr, true));
+    .then(arr => writeToCSV(arr, false))
+    .then(() => console.log('done'));
+  
+  for (let i = 2; i <= 11; i++) {
+    a = a.then(() => read(path.join(__dirname, 'dataPages', `page${i}.html`)))
+      .then(cleanInput)
+      .then(arr => writeToCSV(arr, true))
+      .then(() => console.log('finished ', i));
+  }
 }
+
 
 function cleanInput(val) {
   const $ = cheerio.load(val.toString());
   const elem = $('div.nba-stat-table__overflow table tr');
-  const arr = elem.text().split(`\n          \n          `).map((row) => row.split('\n          '));
-  arr.shift();
-  arr[0].splice(arr[1].length);
-  for (var i = 0; i < arr.length; i++) {
-    let end = arr[i].length - 1;
-    arr[i][end] = arr[i][end].split('\n        ')[0];
-  }
+  const tmp = elem.text().split(`\n        \n          `);
+  const arr = tmp.map((row) => row.split('\n          '));
+  arr[0].splice(0, 1);
+  arr.forEach(row => row.splice(0, 1));
+  let last = arr[arr.length - 1];
+  arr[arr.length - 1][last.length - 1] = arr[arr.length - 1][last.length - 1].split('\n')[0];
   arr.push([]);
+  // arr[0].splice(1,0);
+  // debugger;
+  // const p1 = elem.text().split(`\n          \n           `);
+  // debugger;
+  // arr.shift();
+  // arr[0].splice(arr[1].length);
+  // for (var i = 0; i < arr.length; i++) {
+  //   let end = arr[i].length - 1;
+  //   arr[i][end] = arr[i][end].split('\n        ')[0];
+  // }
+  // arr.push([]);
   return arr;
 }
+`
+        
+`
 
 function writeToCSV(arr, shouldAppend = false) {
   const loc = path.join(__dirname, 'data.csv');
